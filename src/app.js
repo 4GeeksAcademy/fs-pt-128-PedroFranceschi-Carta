a34b03d15b8a8633755482b591f4d9eceffb2d7d
Pedro Franceschi <117317386+FranceschiPedro@users.noreply.github.com>
2025-12-03 12:13:31 +0000

    No for each

--- src/app.js
+++ src/app.js
@@ -5,7 +5,29 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+
+
+
+
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const newNumber = document.getElementById("number");
+  const newPinta = document.getElementsByClassName("pinta")
+
+  const number = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+  const pintaArray = ["♦", "♥", "♠", "♣"]
+
+  let numberDisplay = Math.floor(Math.random() * number.length);
+  let pintaDisplay = Math.floor(Math.random() * pintaArray.length);
+  
+
+  newNumber.textContent = number[numberDisplay]
+
+  newPinta[0].innerHTML = pintaArray[pintaDisplay]
+  newPinta[1].innerHTML = pintaArray[pintaDisplay]
+
+  // newPinta.forEach(element => {
+  //   element.textContent = pintaArray[pintaDisplay]
+  // });
+
 };
